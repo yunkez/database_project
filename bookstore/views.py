93949c37341e12f7d4db9f81443b24a889cefeb2
yunkez <zhouyunke@zhou.local>
2015-11-16 20:41:17 +0800

    show book title

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def index(request):
+    book_list = Book.objects.order_by('title')
+#    output = ', '.join([b.title for b in book_list])
+#    return HttpResponse(output)
+
+    context = {'book_list': book_list}
+    return render(request, 'bookstore/login.html', context)
