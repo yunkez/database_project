15cf7e798a6d2754f08a2d5d1d38bf7f5d0ef2a7
linyijuan <linyijuan22@gmail.com>
2015-11-17 21:25:17 +0800

    aba

--- bookstore/static/bookstore/app/app.js
+++ bookstore/static/bookstore/app/app.js
@@ -1,14 +1,45 @@
-app = angular.module('myApp', [])
+app = angular.module('myApp', ['ui.bootstrap'])
 
 app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
 
-app.controller( 'AppController', ['$scope', '$http', function($scope, $http) {
+app.controller( 'AppController', ['$scope','$modal', '$http', function($scope,$modal, $http) {
     $http.get('/bookstore/all_books').success(function(response) {
         $scope.book_list = response;
     });
 
+    $scope.openBookModal = function(book){
+    	var modalInstance = $modal.open({
+            templateUrl: 'BookDetailModal.html',
+            controller: 'BookDetailModalCtrl',
+            resolve: {
+                params: function () {
+                    return book;
+                }
+            }
+        });
+        modalInstance.result.then(
+			function (result) {
+				
+			},
+			function (result) {
+			}
+		);
 
+    };
+
+
+}]);
+
+app.controller('BookDetailModalCtrl', ['$scope', '$modalInstance', 'params', function($scope, $modalInstance, params){
+	$scope.c = params;
+	$scope.ok = function () {
+        $modalInstance.close('this is result for close');
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('this is result for dismiss');
+    };
 }]);
