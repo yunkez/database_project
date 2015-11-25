a8291fe2db9952b0d7f83b593e81dca21ad2be09
linyijuan <linyijuan22@gmail.com>
2015-11-25 21:14:16 +0800

    abc

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	ISBN = models.BigIntegerField()
+	title = models.CharField(max_length=100)
+	publisher = models.CharField(max_length=100)
+	author = models.CharField(max_length=100)
+	keywords = models.CharField(max_length=100)
+	subject = models.CharField(max_length=100)
+	format = models.CharField(max_length=9,
+	        choices=(
+	            ('hardcover', 'hardcover'),
+	            ('softcover', 'softcover'),
+	        )
+	    )
+	year_of_publication = models.PositiveIntegerField()
+	price = models.PositiveIntegerField()
+	copies = models.PositiveIntegerField()
+	def __unicode__(self):
+		return self.title
+
