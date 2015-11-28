8aca76b6345d6009fea72ddfd457ed033b5da6e5
linyijuan <linyijuan22@gmail.com>
2015-11-28 23:56:57 +0800

    navbar

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render,render_to_response, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from django.core.context_processors import csrf
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
@@ -20,8 +18,8 @@ def index(request):
         columns = [col[0] for col in cur.description]
         book_list = [dict(zip(columns, row)) for row in cur.fetchall()]
     except:
-        book_list = ""
-    return render(request, 'bookstore/index.html',{'book_list': book_list,'login': True})
+        book_list = ""    
+    return render(request, 'bookstore/index.html',{'book_list': book_list,'base_template':'base_auth.html'})
 
 def user_login(request):
     context = RequestContext(request)
@@ -38,7 +36,7 @@ def user_login(request):
         else:
             return HttpResponse("You have entered wrong username or password.")
     else:
-        return render_to_response('bookstore/login.html', {'login':False}, context)
+        return render_to_response('bookstore/login.html', {'base_template':'base.html'}, context)
 
 def register(request):
     context = RequestContext(request)
@@ -63,7 +61,7 @@ def register(request):
             print user_form.errors
     else:
         user_form = CustomerCreationForm()
-    return render_to_response('bookstore/signup.html',{'user_form': user_form, 'registered': registered,'login': False},context)
+    return render_to_response('bookstore/signup.html',{'user_form': user_form, 'registered': registered,'base_template':'base.html'},context)
 
 def logoutView(request, onsuccess='/login', onfail='/bookstore'):
     if request.user.is_authenticated():
