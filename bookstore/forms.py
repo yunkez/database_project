b0ffd1f3637ec2671e55e0ee9e41978491888ab5
linyijuan <linyijuan22@gmail.com>
2015-11-30 22:57:34 +0800

    ui

--- bookstore/forms.py
+++ bookstore/forms.py
@@ -3,12 +3,19 @@ from .models import Customer
 from django import forms
 
 class CustomerCreationForm(forms.ModelForm):
-	password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-	password2 = forms.CharField(label="Confirm your password",widget=forms.PasswordInput)
+	password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
+	password2 = forms.CharField(label="Confirm your password",widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'confirm your password'}))
 
 	class Meta:
 		model = Customer
 		fields = ['username','fullname','phone','card','address']
+		widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'login name','required': True}),
+            'fullname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'full name','required': True}),
+            'phone': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'phone number','required': True}),
+            'card': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'card number','required': True}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'address','required': True}),
+        }
 
 	def clean_password2(self):
 		password1 = self.cleaned_data.get("password1")
@@ -29,6 +36,12 @@ class CustomerChangeForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['fullname','phone','card','address']
+        widgets = {
+            'fullname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'full name'}),
+            'phone': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'phone number'}),
+            'card': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'card number'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'address'}),
+        }
 
 	def clean_password(self):
 		return ""
